521caeec2aac87798aa450e3a8bbd25c42fc7203
Ruigu <ruigukev@gmail.com>
2012-08-15 09:34:19 +0300

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AimsellSfa::Application.config.secret_token = 'e03c7948aab688b53af9f9aff37451154c0d6758f797f2fba15f6d3b16eef78e6c58591e9a3323c96d5f2201b3d8e28eff1296bf84b123e202db901abe01bba6'
